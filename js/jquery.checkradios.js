fa6b10ed980820ee5c8e5c38d106de076f86d907
cosmicwheels <cosmicwheels27@gmail.com>
2014-03-26 17:01:55 +0000

    Updated License

--- js/jquery.checkradios.js
+++ js/jquery.checkradios.js
@@ -3,13 +3,7 @@
 	by Chris McGuckin (https://github.com/cosmicwheels)
 	
 	
-	License: This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License as published
-	by the Free Software Foundation; either version 3 of the License, or (at
-	your option) any later version. This program is distributed in the hope
-	that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+	License: MIT (http://opensource.org/licenses/MIT)
 	
 	
 	Credits:
